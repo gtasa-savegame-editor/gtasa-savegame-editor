daf049b68880a9180c34d450d1da4eea7d1f870d
s.hardt <s.hardt@micromata.de>
2018-10-20 00:29:03 +0200

    #11 Start adding car and garage stuff

---
+++ src/main/java/nl/paulinternet/gtasaveedit/model/savegame/blocks/Block03.java
@@ -0,0 +1,138 @@
+package nl.paulinternet.gtasaveedit.model.savegame.blocks;
+
+import static nl.paulinternet.gtasaveedit.model.Model.vars;
+
+import nl.paulinternet.gtasaveedit.model.exceptions.FileFormatException;
+import nl.paulinternet.gtasaveedit.model.link.Link;
+import nl.paulinternet.gtasaveedit.model.link.LinkArray;
+import nl.paulinternet.gtasaveedit.model.link.LinkFloat;
+import nl.paulinternet.gtasaveedit.model.link.LinkInt;
+import nl.paulinternet.gtasaveedit.model.savegame.SavegameData;
+
+
+/**
+ * @author Sebastian Hardt (s.hardt@micromata.de) Date: 19.10.18 Time: 17:07
+ */
+public class Block03 extends LinkArray {
+
+  public Block03() {
+    setLinks(new Link[]{
+        new LinkFloat(vars.health, 2, 0x20),
+        new LinkFloat(vars.armor, 2, 0x24),
+        new LinkInt(vars.weaponStartSlot, 2, 0x224, 1),
+    });
+  }
+
+  @Override
+  public void load(SavegameData io) throws FileFormatException {
+    super.load(io);
+
+    // lets try something here
+    for (int i = 0; i < 80; i++) {
+
+      int size = 64;
+
+      int pos = 0x27 + size * i;
+
+      byte[] bytes = io.getBlock(3).getBytes(pos, pos + size);
+
+      final float xPos = io.readFloat(3, pos + 0);
+      final float yPos = io.readFloat(3, pos + 4);
+      final float zPos = io.readFloat(3, pos + 8);
+
+      final int f1 = io.readByte(3, pos + 12);
+      final int f2 = io.readByte(3, pos + 13);
+      final int f3 = io.readByte(3, pos + 14);
+      final int f4 = io.readByte(3, pos + 15);
+      final int f5 = io.readByte(3, pos + 16);
+      final int f6 = io.readByte(3, pos + 17);
+
+      final int type = io.readInt(3, pos + 18, 2);
+
+      final int mod1 = io.readInt(3, pos + 20, 2);
+      final int mod2 = io.readInt(3, pos + 22, 2);
+      final int mod3 = io.readInt(3, pos + 24, 2);
+      final int mod4 = io.readInt(3, pos + 26, 2);
+      final int mod5 = io.readInt(3, pos + 28, 2);
+      final int mod6 = io.readInt(3, pos + 30, 2);
+      final int mod7 = io.readInt(3, pos + 32, 2);
+      final int mod8 = io.readInt(3, pos + 34, 2);
+      final int mod9 = io.readInt(3, pos + 36, 2);
+      final int mod10 = io.readInt(3, pos + 38, 2);
+      final int mod11 = io.readInt(3, pos + 40, 2);
+      final int mod12 = io.readInt(3, pos + 42, 2);
+      final int mod13 = io.readInt(3, pos + 44, 2);
+      final int mod14 = io.readInt(3, pos + 46, 2);
+      final int mod15 = io.readInt(3, pos + 48, 2);
+
+      final int color1 = io.readByte(3, pos + 50);
+      final int color2 = io.readByte(3, pos + 51);
+
+      final int radioStation = io.readByte(3, pos + 54);
+
+      final int paintJob = io.readByte(3, pos + 58);
+      final int nitro = io.readByte(3, pos + 59);
+
+      final int x = io.readByte(3, pos + 60);
+      final int y = io.readByte(3, pos + 61);
+      final int z = io.readByte(3, pos + 62);
+
+      final String iout = (i < 10) ? " " + i : String.valueOf(i);
+
+      final String debugStr =
+          "Car: " + iout + " type: " + type + "  (" + xPos + "," + yPos + "," + zPos + ") has nitro: " + nitro
+              + " paintJob: " + paintJob + " radioStation: " + Integer.toHexString(radioStation) + " angle: (" + x + ","
+              + y + "," + z + ") c1: " + color1 + " c2: " + color2;
+
+      String fString = "";
+      for (int j = 0; j < 6; j++) {
+        final int fst = io.readByte(3, pos + 12 + j);
+        if (fst != 0) {
+          fString += " -> F" + j + ": = " + fst;
+        }
+      }
+
+      String modsString = "";
+      for (int j = 0; j < 15; j++) {
+        final int mod = io.readInt(3, pos + 20 + j * 2, 2);
+        if (mod != 65535 && mod != 0) {
+          modsString += "-> Mod_" + j + ": = " + mod;
+        }
+      }
+
+      System.out.println(debugStr + fString + modsString);
+    }
+
+    //http://gta.wikia.com/wiki/Garage
+    ////https://github.com/goodidea82/GarageExtender/tree/master/Source
+    int garageOffset = 5159;
+
+    for (int i = 0; i < 50; i++) {
+      int size = 80;
+
+      int pos = garageOffset + size * i;
+
+      byte[] bytes = io.getBlock(3).getBytes(pos, pos + size);
+
+      final byte[] nameBytes = getBytes(68, 76, bytes);
+
+      System.out.println("Garage: " + i + " name: " new String(nameBytes));
+
+
+    }
+
+  }
+
+  public byte[] getBytes(int begin, int end, byte[] array) {
+
+    // Create a new byte array
+    byte[] data = new byte[end - begin];
+
+    for (int i = 0; i < end - begin; i++) {
+      data[i] = array[begin + i];
+    }
+
+    // Return
+    return data;
+  }
+}
