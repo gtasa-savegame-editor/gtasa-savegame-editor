c4c31d1544f80c1a078df979f4e53954bdc094d8
Lukas Fülling <lukas@k40s.net>
2018-11-25 17:54:12 +0100

    fix server, add better logging, start work on parsing android savegames

--- src/main/java/nl/paulinternet/gtasaveedit/extractor/ExtractorServer.java
+++ src/main/java/nl/paulinternet/gtasaveedit/extractor/ExtractorServer.java
@@ -1,27 +1,25 @@
 package nl.paulinternet.gtasaveedit.extractor;
 
 import com.sun.net.httpserver.HttpServer;
+import nl.paulinternet.gtasaveedit.view.Main;
 import nl.paulinternet.gtasaveedit.view.menu.extractor.ExtractorMenu;
 import nl.paulinternet.gtasaveedit.view.window.MainWindow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import javax.swing.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class ExtractorServer extends Thread {
 
-    public static final String PROTO_VERSION = "1";
+    private static final Logger log = LoggerFactory.getLogger(ExtractorServer.class);
+    private static final String PROTO_VERSION = "1";
     private static HttpServer server = null;
     private static Path tempDir = null;
     private ExtractorMenu menu;
@@ -40,24 +38,27 @@ public class ExtractorServer extends Thread {
                 JOptionPane.showMessageDialog(MainWindow.getInstance(), e.getMessage(), "Unable to start savegame extractor server!", JOptionPane.ERROR_MESSAGE);
             }
         } else {
-            System.err.println("Server already running! " + server.getAddress().toString());
+            log.error("Server already running! " + server.getAddress().toString());
         }
     }
 
     private void startServer() throws IOException {
-        System.out.println("Starting server...");
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        log.info("Starting server on '" + hostAddress + "'");
         tempDir = Files.createTempDirectory("gtasaseExtractor");
-        server = HttpServer.create(new InetSocketAddress(0), 0);
+        server = HttpServer.create(new InetSocketAddress(hostAddress,0), 0);
         server.createContext("/add", new FormDataHandler(d -> {
             Object[] fileData = d.toArray();
+            //noinspection ForLoopReplaceableByForEach it's prettier this way
             for (int i = 0; i < fileData.length; i++) {
                 FormDataHandler.FileData f = (FormDataHandler.FileData) fileData[i];
                 if (f.contentType.equals("application/octet-stream")) {
                     File savegameFile = new File(tempDir.toFile().getAbsolutePath() + File.separator + f.fileName);
                     try (FileOutputStream stream = new FileOutputStream(savegameFile)) {
-                        System.out.println("Writing file: '" + savegameFile.getAbsolutePath() + "'");
+                        log.info("Writing file: '" + savegameFile.getAbsolutePath() + "'");
                         stream.write(f.data);
                         ExtractedSavegameHolder.addSavegame(new ExtractedSavegameFile(savegameFile, f.fileName), menu);
+                        JOptionPane.showMessageDialog(MainWindow.getInstance(), "Received file: '" + savegameFile.getName() + "' successfully.", "Savegame Received", JOptionPane.INFORMATION_MESSAGE);
                     } catch (IOException e) {
                         JOptionPane.showMessageDialog(MainWindow.getInstance(), e.getMessage(), "Unable to write temp file!", JOptionPane.ERROR_MESSAGE);
                     }
@@ -107,29 +108,32 @@ public class ExtractorServer extends Thread {
         });
         server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
         server.start();
-        if(jmdns == null) {
-            jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+        if (jmdns == null) {
+            jmdns = JmDNS.create(hostAddress);
         }
         HashMap<String, String> props = new HashMap<>();
         props.put("version", PROTO_VERSION);
+        props.put("ip", hostAddress);
+        props.put("port", String.valueOf(server.getAddress().getPort()));
         try {
             props.put("hostname", InetAddress.getLocalHost().getHostName());
         } catch (UnknownHostException e) {
             JOptionPane.showMessageDialog(MainWindow.getInstance(), e.getMessage(), "Unable to get hostname!", JOptionPane.ERROR_MESSAGE);
         }
-        ServiceInfo serviceInfo = ServiceInfo.create("_gtasa-se._tcp.local.", "extractor", server.getAddress().getPort(), 0, 0, props);
+        ServiceInfo serviceInfo = ServiceInfo.create("_gtasa-se._tcp.local.", hostAddress.replaceAll("\\.","-"), server.getAddress().getPort(), 0, 0, props);
         jmdns.registerService(serviceInfo);
     }
 
     public void stopServer() {
         if (server != null) {
-            System.out.println("Stopping server...");
+            log.info("Stopping server...");
             server.stop(0);
             jmdns.unregisterAllServices();
             server = null;
             tempDir = null;
         } else {
-            System.out.println("Server already stopped!");
+            log.warn("Server already stopped!");
         }
     }
 
